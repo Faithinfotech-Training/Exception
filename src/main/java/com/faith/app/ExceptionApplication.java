0988470531e68b6a6d88678d3a6cd99b14a36d69
praveenavm <praveena.vm@faithinfotech.in>
2022-05-06 18:14:30 +0530

    added a print which states my name

--- src/main/java/com/faith/app/ExceptionApplication.java
+++ src/main/java/com/faith/app/ExceptionApplication.java
@@ -9,6 +9,7 @@ public class ExceptionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExceptionApplication.class, args);
 		System.out.println("Git");
+		System.out.println("praveena");
 	}
 
 }
